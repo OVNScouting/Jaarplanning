c2ba57e92d5e80e1d0a324d01c5ec3f79eb462cd
OVN Scouting 48 <ovnscouting@gmail.com>
2025-12-02 20:17:44 +0100

    Update dashboard.js

--- dashboard.js
+++ dashboard.js
@@ -15,7 +15,6 @@ const firebaseConfig = {
   authDomain: "ovn-jaarplanning.firebaseapp.com",
   databaseURL: "https://ovn-jaarplanning-default-rtdb.europe-west1.firebasedatabase.app",
   projectId: "ovn-jaarplanning",
-  storageBucket: "ovn-jaarplanning.firebasestorage.app",
   messagingSenderId: "311108828430",
   appId: "1:311108828430:web:40f3564fca975423972b5f"
 };

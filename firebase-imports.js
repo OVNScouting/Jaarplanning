bc1031326020fc31cf4e8ec3bf0b98c3cb1dcc83
OVN Scouting 48 <ovnscouting@gmail.com>
2025-12-03 12:13:06 +0100

    Create firebase-imports.js

---
+++ firebase-imports.js
@@ -0,0 +1,11 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-app.js";
+import {
+  getDatabase,
+  ref,
+  get,
+  set,
+  update,
+  push
+} from "https://www.gstatic.com/firebasejs/10.7.1/firebase-database.js";
+
+export { initializeApp, getDatabase, ref, get, set, update, push };

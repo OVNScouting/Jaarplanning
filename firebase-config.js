f27ea4ee9d3273c9281a530a82e817a94539ee18
OVN Scouting 48 <ovnscouting@gmail.com>
2025-12-03 12:12:34 +0100

    Create firebase-config.js

---
+++ firebase-config.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyCFQeno5rmLIvZdscjrimvFO7ZsJW7qBTM",
+  authDomain: "ovn-jaarplanning.firebaseapp.com",
+  databaseURL: "https://ovn-jaarplanning-default-rtdb.europe-west1.firebasedatabase.app",
+  projectId: "ovn-jaarplanning",
+  storageBucket: "ovn-jaarplanning.firebasestorage.app",
+  messagingSenderId: "311108828430",
+  appId: "1:311108828430:web:40f3564fca975423972b5f"
+};

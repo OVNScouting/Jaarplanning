62ebba6ac5c0b869ca4037b6ca59ead36ad50390
OVN Scouting 48 <ovnscouting@gmail.com>
2025-12-03 12:15:53 +0100

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,4 +1,4 @@
-const firebaseConfig = {
+window.firebaseConfig = {
   apiKey: "AIzaSyCFQeno5rmLIvZdscjrimvFO7ZsJW7qBTM",
   authDomain: "ovn-jaarplanning.firebaseapp.com",
   databaseURL: "https://ovn-jaarplanning-default-rtdb.europe-west1.firebasedatabase.app",

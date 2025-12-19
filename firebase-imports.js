9f22c2e725a149ed91203af5e0a9f5159360f807
OVN Scouting 48 <ovnscouting@gmail.com>
2025-12-19 14:04:47 +0100

    Update firebase-imports.js

--- firebase-imports.js
+++ firebase-imports.js
@@ -1,12 +1,29 @@
-export {
-  initializeApp
-} from "https://www.gstatic.com/firebasejs/10.7.1/firebase-app.js";
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.23.0/firebase-app.js";
+import { 
+  getDatabase, 
+  ref, 
+  get, 
+  set, 
+  update, 
+  push, 
+  remove 
+} from "https://www.gstatic.com/firebasejs/9.23.0/firebase-database.js";
+
+// Firebase Authentication (FASE 0: alleen beschikbaar maken, nog niet gebruiken)
+import { 
+  getAuth 
+} from "https://www.gstatic.com/firebasejs/9.23.0/firebase-auth.js";
+
 
 export {
+  initializeApp,
   getDatabase,
   ref,
   get,
   set,
   update,
-  push
-} from "https://www.gstatic.com/firebasejs/10.7.1/firebase-database.js";
+  push,
+  remove,
+  getAuth // FASE 0: nog niet actief gebruikt
+};
+
